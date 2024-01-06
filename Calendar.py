3885b30cd24ac532e81d34e7a139c49574a1350a
Shashank Karan <shashank.karan25@gmail.com>
2024-01-06 18:16:36 +0530

    Calender

---
+++ Calendar.py
@@ -0,0 +1,13 @@
+# Make one Month Calendar
+import calendar
+yy = 2023 #year
+mm = 9 #month
+print(calendar.month(yy, mm))
+
+#-------------------------------------------------------------#
+#-------------------------------------------------------------#
+
+# Make one Year Calendar
+import calendar
+for i in range(1, 13):
+    print(calendar.month(2023, i))
